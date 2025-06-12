b391e6d692c0d444109dffcf506736d97202468d
alix-leon <alixlm19@hotmail.com>
2025-06-12 11:16:27 -0400

    Started crawleer implementation

--- llm_twin/crawlers/base.py
+++ llm_twin/crawlers/base.py
@@ -1,11 +1,10 @@
-# import chromedriver_autoinstaller
-
 from abc import ABC, abstractmethod
-from tempfile import mkdtemp
 from typing import Any, TypeVar
 
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
+
+_DriverType = TypeVar("_DriverType", bound=WebDriver)
 
 
 class BaseCrawler(ABC):
@@ -28,31 +27,7 @@ class BaseSeleniumCrawler(BaseCrawler, ABC):
         ),
     )
 
-    _DriverType = TypeVar("_DriverType", bound=WebDriver)
-
-    def __init__(self, scroll_limit: int = 5) -> None:
-        super().__init__()
-
-        options: BaseSeleniumCrawler._DriverOptionsType = webdriver.ChromeOptions()
-
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless=new")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--log-level=3")
-        options.add_argument("--disable-popup-blocking")
-        options.add_argument("--disable-notifications")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-background-networking")
-        options.add_argument("--ignore-certificate-errors")
-        options.add_argument(f"--user-data-dir={mkdtemp()}")
-        options.add_argument(f"--data-path={mkdtemp()}")
-        options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-        options.add_argument("--remote-debugging-port=9226")
-
-        self.set_extra_driver_options(options)
-
-        self.scroll_limit: int = scroll_limit
-        self.driver: BaseSeleniumCrawler._DriverType = webdriver.Chrome(options=options)
-
-    def set_extra_driver_options(self, options: _DriverOptionsType) -> None:
-        pass
+    def set_options(
+        self, options: list[str], options_handler: _DriverOptionsType
+    ) -> None:
+        self.options = options_handler(options)
--- llm_twin/crawlers/selenium_crawler.py
+++ llm_twin/crawlers/selenium_crawler.py
@@ -1,8 +1,39 @@
+from selenium import webdriver
+
 from .base import BaseSeleniumCrawler
 
 
 class SeleniumCrawler(BaseSeleniumCrawler):
+    def __init__(self, scroll_limit: int = 5) -> None:
+        super().__init__()
+
+        options: BaseSeleniumCrawler._DriverOptionsType = webdriver.ChromeOptions()
+        self.set_extra_driver_options(options)
+
+        self.scroll_limit: int = scroll_limit
+        self.driver: _DriverType = webdriver.Chrome(options=options)
+
     @classmethod
     def build(cls) -> "SeleniumCrawler":
         crawler = cls()
         return crawler
+
+    def attach_chrome_driver(self) -> "SeleniumCrawler":
+        options: list[str] = [
+            "--no-sandbox",
+            "--headless=new",
+            "--disable-dev-shm-usage",
+            "--log-level=3",
+            "--disable-popup-blocking",
+            "--disable-notifications",
+            "--disable-extensions",
+            "--disable-background-networking",
+            "--ignore-certificate-errors",
+            "--user-data-dir={mkdtemp()}",
+            "--data-path={mkdtemp()}",
+            "--disk-cache-dir={mkdtemp()}",
+            "--remote-debugging-port=9226",
+        ]
+        self.set_options(options, webdriver.ChromeOptions)
+
+        return self
