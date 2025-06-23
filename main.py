9cd440bd219d3f715a0df5b55aaae767d850aeff
alix-leon <alixlm19@hotmail.com>
2025-06-23 00:21:47 -0400

    resolve conflicts

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from llm_twin.crawlers.selenium_crawler import SeleniumCrawler
 
 def main():
     print("Hello from llm-twin!")
+<<<<<<< Updated upstream
     # b = BaseSeleniumCrawler()
     # b.from_config()
     # p = b._install_or_get_driver("chrome", install_if_missing=True)
@@ -14,6 +15,17 @@ def main():
     driver = crawler.get_driver()
     driver.get("https://www.google.com")
     print(driver.page_source)
+=======
+    b = BaseSeleniumCrawler()
+    b.from_config()
+    p = b.install_or_get_driver("edge", install_if_missing=True)
+    print(p)
+    s = webdriver.EdgeService(executable_path=p)
+
+    d = webdriver.Edge(options=b.options_handler, service=s)
+    d.get("https://www.google.com")
+    print(d.page_source)
+>>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
