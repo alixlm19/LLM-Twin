61c301e0e2b30295b366a68565e4049b9c5cbc88
Alix Leon <alixlm19@hotmail.com>
2025-06-28 01:21:22 -0400

    Updated dependencies

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-from llm_twin.crawlers.selenium_crawler import SeleniumCrawler
+from llm_twin.crawlers.linkedin import LinkedInCrawler
 
 
 def main():
     print("Hello from llm-twin!")
-<<<<<<< Updated upstream
-    # b = BaseSeleniumCrawler()
-    # b.from_config()
-    # p = b._install_or_get_driver("chrome", install_if_missing=True)
-    # print(p)
-    # s = webdriver.ChromeService(p)
-    # driver = webdriver.Chrome(options=b.options_handler, service=s)
 
-    crawler = SeleniumCrawler().attach_chrome_driver().build()
-    driver = crawler.get_driver()
-    driver.get("https://www.google.com")
-    print(driver.page_source)
-=======
-    b = BaseSeleniumCrawler()
-    b.from_config()
-    p = b.install_or_get_driver("edge", install_if_missing=True)
-    print(p)
-    s = webdriver.EdgeService(executable_path=p)
-
-    d = webdriver.Edge(options=b.options_handler, service=s)
-    d.get("https://www.google.com")
-    print(d.page_source)
->>>>>>> Stashed changes
+    crawler = LinkedInCrawler().attach_chrome_driver().build()
+    crawler.driver.close()
 
 
 if __name__ == "__main__":
