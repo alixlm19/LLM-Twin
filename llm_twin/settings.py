b0683029853937ab0cd50fc6621b94babb9e38eb
Alix Leon <alixlm19@hotmail.com>
2025-06-28 01:20:54 -0400

    started settings implementation

---
+++ llm_twin/settings.py
@@ -0,0 +1,39 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # --- Required settings to work locally: start
+
+    OPENAI_MODEL_ID: str = "gpt-4o-mini"
+    OPENAI_API_KEY: str | None = None
+
+    # Huggingface API
+    HUGGINGFACE_ACCESS_TOKEN: str | None = None
+
+    # Comet ML (training)
+    COMET_API_KEY: str | None = None
+    COMET_PROJECT: str = "twin"
+
+    # --- Required settings to work locally: end
+
+    # LinkedIn Credentials
+    LINKEDIN_USERNAME: str | None = None
+    LINKEDIN_PASSWORD: str | None = None
+
+    @classmethod
+    def load_settings(cls) -> "Settings":
+        """
+        Tries to load the settings from the ZenML secret store. If the secret does not exist,
+        it initialized the settings from the .env file and default values.
+
+        Returns:
+            `Settings:` The initialized settings object.
+        """
+
+        settings = Settings()
+        return settings
+
+
+settings = Settings.load_settings()
